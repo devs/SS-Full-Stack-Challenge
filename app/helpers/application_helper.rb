6ff12f0fd9fe37f5791fb27190833c43607a5130
devin <dpowell7@gmail.com>
2014-05-09 00:14:52 -0400

    Fix bug where only last mention in tweets was linkified

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,11 @@ module ApplicationHelper
   def insert_links(tweet)
     screen_names = extract_mentioned_screen_names(tweet)
     if screen_names != []
-      new_tweet = ""
       screen_names.each do |s|
         link_to_user = "<a href='/user_tweets/" + s + "'>" + s + "</a>"
-        new_tweet = tweet.sub(s, link_to_user)
+        tweet = tweet.gsub(s, link_to_user)
       end
-      new_tweet
+      tweet
     else
       tweet
     end
